2f91772a6d67429f941f0a476e3082e7cc912fb2
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-10 00:40:27 -0500

    favicon problem

--- src/components/SeoHead/Favicons/index.tsx
+++ src/components/SeoHead/Favicons/index.tsx
@@ -1,27 +1,7 @@
 export const Favicons = () => {
   return (
     <>
-      <link rel="icon" href="/favicon/favicon.png" type="image/x-icon" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        // href="/favicon/apple-touch-icon.png"
-        href="/favicon/favicon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        // href="/favicon/favicon-32x32.png"
-        href="/favicon/favicon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        // href="/favicon/favicon-16x16.png"
-        href="/favicon/favicon.png"
-      />
+      <link rel="icon" href="/favicon.png" type="image/png" />
     </>
   );
 };
