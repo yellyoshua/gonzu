d1c1928e86055bdb3e4af61e4fe07a5b2a48eb42
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-10 02:05:45 -0500

    under-maintenance image size

--- src/ui/UnderMaintenance/index.tsx
+++ src/ui/UnderMaintenance/index.tsx
@@ -3,12 +3,12 @@ interface UnderMaintenanceProps {}
 export const UnderMaintenance = ({}: UnderMaintenanceProps) => {
   return (
     <section className="mx-8 my-10 py-10">
-      <h3 className="font-jost text-black text-3xl text-center">
-        En mantenimiento, disculpa las molestias
+      <h3 className="font-jost text-black font-bold text-3xl text-center">
+        En mantenimiento, en breve estaremos en l&iacute;nea.
       </h3>
       <div className="mt-10 w-full">
         <img
-          className="m-auto max-w-sm"
+          className="m-auto sm:max-w-sm"
           src="/assets/svg/under_maintenance.svg"
           alt="under_maintenance"
         />
