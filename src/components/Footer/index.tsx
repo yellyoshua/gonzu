1869c62fe07fb45ce64d8dcd237ebf88e8ed4adf
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-10 02:05:27 -0500

    change footer bg appareance

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterSections } from "./FooterSections";
 
 export function Footer() {
   return (
-    <footer className="py-10 sm:my-10 bg-gray-200">
+    <footer className="py-10 sm:my-10 border-t-2 border-gray-300">
       <FooterSections />
       <Copyright className="mt-10" copyright={ApplicationCopyright} />
     </footer>
