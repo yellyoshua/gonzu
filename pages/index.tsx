eca7192f6a60db0021d2f4871a6db6894af38c76
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-13 02:02:51 -0500

    vercel ci

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { Layout } from "@/app/ui/Layout";
 import { HomeContent } from "@/app/entities/gonzu/ui/HomeContent";
-import { UnderMaintenance } from "@/app/ui/UnderMaintenance";
 
 export default function HomePage() {
   return (
