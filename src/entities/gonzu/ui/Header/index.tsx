096cf119005e6eb2db8ac91b68ae2e6e74f132a8
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-17 00:25:39 -0500

    responsive navbar

--- src/entities/gonzu/ui/Header/index.tsx
+++ src/entities/gonzu/ui/Header/index.tsx
@@ -1,18 +1,38 @@
 import shallow from "zustand/shallow";
+import { useMedia } from "react-use";
 import { useGonzuStore } from "@/app/entities/gonzu/flux/gonzu.store";
 import { LogoBanner } from "@/app/entities/gonzu/components/LogoBanner";
 import { NavBar } from "@/app/entities/gonzu/components/NavBar";
+import { DrawerNavbar } from "@/app/entities/gonzu/components/DrawerNavbar";
 
 export const GonzuHeader = () => {
-  const { links, logosBanner } = useGonzuStore(
-    (state) => ({ links: state.header.links, logosBanner: state.bannerLogo }),
+  const isMDBreakpoint = useMedia("(min-width: 768px)", true);
+
+  const { links, logosBanner, copyright } = useGonzuStore(
+    (state) => ({
+      links: state.header.links,
+      logosBanner: state.bannerLogo,
+      copyright: state.copyright,
+    }),
     shallow
   );
 
+  console.log({ isMDBreakpoint });
+
   return (
-    <header className="w-full my-4">
-      <LogoBanner logoBanner={logosBanner} />
-      <NavBar links={links} />
+    <header className={`w-full relative ${isMDBreakpoint ? "h-full" : "h-20"}`}>
+      {isMDBreakpoint ? (
+        <NavBar
+          logoElement={<LogoBanner logoBanner={logosBanner} />}
+          links={links}
+        />
+      ) : (
+        <DrawerNavbar
+          links={links}
+          copyright={copyright}
+          logoElement={<LogoBanner logoBanner={logosBanner} className="h-16" />}
+        />
+      )}
     </header>
   );
 };
