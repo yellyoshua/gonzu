a535f4176442922b11f4066a0216c21682a940f5
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-20 21:59:23 -0500

    implementation

--- src/entities/pages/ui/PageContent/index.tsx
+++ src/entities/pages/ui/PageContent/index.tsx
@@ -1,15 +1,18 @@
-import { GraphCMSMarkdown } from "@/app/components/GraphCMSMarkdown";
 import { RawRichText } from "@/app/interfaces";
-import { usePageStore } from "../../flux/pages.store";
+import { GraphCMSMarkdown } from "@/app/components/GraphCMSMarkdown";
+import { PagesBackdrop } from "@/app/entities/pages/components/PagesBackdrop";
+import { usePageStore } from "@/app/entities/pages/flux/pages.store";
 
 interface PageContentProps {}
 
 export const PageContent = ({}: PageContentProps) => {
   const page = usePageStore((state) => state.page);
   const safeMarkdownContent = page?.content! as RawRichText;
+  const safeBackdrop = page ? page.backdrop : null;
 
   return (
     <div>
+      <PagesBackdrop backdrop={safeBackdrop} />
       <GraphCMSMarkdown
         className="max-w-screen-lg px-5 m-auto"
         richTextProps={{ content: safeMarkdownContent.raw }}
