ac49cbaf5e7d7e5ab62e4b2e696e0f513fa07ff8
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-20 21:49:04 -0500

    page backdrop interface (Asset)

--- src/entities/pages/interfaces/index.ts
+++ src/entities/pages/interfaces/index.ts
@@ -1,9 +1,10 @@
-import { LinkUrl, RawRichText } from "@/app/interfaces";
+import { Asset, LinkUrl, RawRichText } from "@/app/interfaces";
 
 export interface Page {
   slug: string;
   title: string;
   content: RawRichText | { html: string } | null;
+  backdrop: Asset | null;
 }
 
 export interface PageStore {
