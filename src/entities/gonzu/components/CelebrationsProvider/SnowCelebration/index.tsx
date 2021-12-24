f5cfe79f6df45d103503703549185e3d3e3b8860
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-24 02:50:29 -0500

    SnowCelebratiion component (confetti)

---
+++ src/entities/gonzu/components/CelebrationsProvider/SnowCelebration/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { usePageLeave } from "react-use";
+import { useConfetti } from "@/app/hooks/useConfetti";
+
+interface SnowCelebrationProps {
+  execute: boolean;
+  timeOut?: number;
+}
+
+export const SnowCelebration = ({
+  execute = false,
+  timeOut,
+}: SnowCelebrationProps) => {
+  const { canvasConfettiRef, confetti, resetConfetti } = useConfetti({
+    resize: true,
+    useWorker: true,
+  });
+
+  usePageLeave(() => resetConfetti(true));
+
+  useEffect(() => {
+    if (execute) {
+      confetti.snow(timeOut);
+    }
+
+    return () => {
+      resetConfetti();
+    };
+  }, [execute, timeOut]);
+
+  return (
+    <canvas
+      ref={canvasConfettiRef}
+      className="fixed top-0 bottom-0 pointer-events-none h-screen w-full"
+    />
+  );
+};
