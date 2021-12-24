55766f298e32d8f85a53888a261b209465332ee9
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-24 02:50:42 -0500

    FireworksCelebratiion component (confetti)

---
+++ src/entities/gonzu/components/CelebrationsProvider/FireworksCelebration/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { usePageLeave } from "react-use";
+import { useConfetti } from "@/app/hooks/useConfetti";
+
+interface FireworksCelebrationProps {
+  execute: boolean;
+  timeOut?: number;
+}
+
+export const FireworksCelebration = ({
+  execute = false,
+  timeOut,
+}: FireworksCelebrationProps) => {
+  const { canvasConfettiRef, confetti, resetConfetti } = useConfetti({
+    resize: true,
+  });
+
+  usePageLeave(() => resetConfetti(true));
+
+  useEffect(() => {
+    if (execute) {
+      confetti.fireworks(timeOut);
+    }
+
+    return () => {
+      resetConfetti();
+    };
+  }, [execute, timeOut]);
+
+  return (
+    <canvas
+      ref={canvasConfettiRef}
+      className="fixed top-0 bottom-0 pointer-events-none h-screen w-full"
+    />
+  );
+};
