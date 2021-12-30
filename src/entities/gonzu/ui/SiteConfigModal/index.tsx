ecfa4488fbaff2b222a6e63fa4fb5922224ebf62
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-29 23:59:18 -0500

    float button change color on mobile

--- src/entities/gonzu/ui/SiteConfigModal/index.tsx
+++ src/entities/gonzu/ui/SiteConfigModal/index.tsx
@@ -5,11 +5,13 @@ import BeakerIcon from "@heroicons/react/outline/BeakerIcon";
 import { DarkModeSwitch } from "../../components/DarkModeSwitch";
 import { CelebrationsRadioPicker } from "../../components/CelebrationsRadioPicker";
 import { SongsRadioPicker } from "../../components/SongsRadioPicker";
+import { useMedia } from "react-use";
 
 interface SiteConfigModalProps {}
 
 export const SiteConfigModal = ({}: SiteConfigModalProps) => {
   const [showModal, setShowModal] = useState(false);
+  const isMDBreakpoint = useMedia("(min-width: 768px)", true);
 
   const closeModalHandler = () => setShowModal(false);
 
@@ -47,7 +49,11 @@ export const SiteConfigModal = ({}: SiteConfigModalProps) => {
       />
       <FloatButton
         onClick={showModalHandler}
-        className="animate-bounce shadow-lg transition-all bg-red-700 rounded-full w-10 h-10 flex items-center text-white hover:text-red-700 justify-center hover:bg-white border-2 border-red-700"
+        className={`animate-bounce shadow-lg transition-all ${
+          isMDBreakpoint
+            ? "bg-red-700 text-white hover:text-red-700 hover:bg-white"
+            : "bg-white text-red-700 hover:text-white hover:bg-red-700"
+        } border-red-700 rounded-full w-10 h-10 flex items-center justify-center border-2`}
       >
         <BeakerIcon className="" width={25} height={25} />
       </FloatButton>
