9591baf78a56d8b7991dd204dc0382e205404601
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-29 23:30:33 -0500

    fix modal view in mobile

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -27,7 +27,7 @@ export const Modal = ({
         className="fixed z-10 inset-0 overflow-y-auto"
         onClose={onPressCancel.handler}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -43,9 +43,7 @@ export const Modal = ({
           <span
             className="hidden sm:inline-block sm:align-middle sm:h-screen"
             aria-hidden="true"
-          >
-            &#8203;
-          </span>
+          />
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -55,7 +53,7 @@ export const Modal = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="w-full inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
