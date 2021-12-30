19a1b142bbec6e5e223450db54e2619841469640
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-30 02:28:57 -0500

    extends tailwind prose dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  preserveHtmlElements: false,
   theme: {
     extend: {
       fontFamily: {
@@ -16,6 +17,69 @@ module.exports = {
         darkTextBold: colors.white,
         darkTextParagraph: colors.gray["200"],
       },
+      typography: (theme) => ({
+        dark: {
+          css: [
+            {
+              color: theme("colors.gray.400"),
+              '[class~="lead"]': {
+                color: theme("colors.gray.300"),
+              },
+              a: {
+                color: theme("colors.white"),
+              },
+              strong: {
+                color: theme("colors.white"),
+              },
+              "ol > li::before": {
+                color: theme("colors.gray.400"),
+              },
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.600"),
+              },
+              hr: {
+                borderColor: theme("colors.gray.200"),
+              },
+              blockquote: {
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.600"),
+              },
+              h1: {
+                color: theme("colors.white"),
+              },
+              h2: {
+                color: theme("colors.white"),
+              },
+              h3: {
+                color: theme("colors.white"),
+              },
+              h4: {
+                color: theme("colors.white"),
+              },
+              "figure figcaption": {
+                color: theme("colors.gray.400"),
+              },
+              code: {
+                color: theme("colors.white"),
+              },
+              "a code": {
+                color: theme("colors.white"),
+              },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
+              },
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
+              },
+            },
+          ],
+        },
+      }),
     },
   },
   variants: {
@@ -24,6 +88,7 @@ module.exports = {
   plugins: [require("@tailwindcss/typography")],
   corePlugins: {
     invert: true,
+    preserveHtmlElements: false,
     float: false,
   },
 };
