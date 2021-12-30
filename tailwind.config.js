203dfb6552f7d888ea4fbefcdb6c22acce861be8
yellyoshua <usuario1529yoshua@hotmail.com>
2021-12-30 01:31:44 -0500

    extends colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
@@ -6,6 +8,13 @@ module.exports = {
       fontFamily: {
         jost: ["Jost", "sans-serif"],
         lato: ["Lato", "sans-serif"],
+      },
+      colors: {
+        darkPrimarySoft: colors.gray["700"],
+        darkPrimary: colors.gray["800"],
+        darkPrimaryBold: colors.gray["900"],
+        darkTextBold: colors.white,
+        darkTextParagraph: colors.gray["200"],
       },
     },
   },
