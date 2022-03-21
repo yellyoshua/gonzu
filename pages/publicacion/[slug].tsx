4a57565d7602b97b75a10fdf61138bdfdc1fbb62
yellyoshua <usuario1529yoshua@hotmail.com>
2022-03-20 21:15:19 -0500

    add post page by slug

---
+++ pages/publicacion/[slug].tsx
@@ -0,0 +1,49 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Layout } from "@/app/ui/Layout";
+import { usePostStore } from "@/app/entities/posts/flux/posts.store";
+import { Post } from "@/app/entities/posts/interfaces";
+import {
+  getPostBySlug,
+  getPostsSlug,
+} from "@/app/entities/posts/flux/posts.actions";
+import { PostContent } from "@/app/entities/posts/ui/PostContent";
+import { Recommendations } from "@/app/components/Recommendations";
+
+interface PostsProps {
+  post: Post;
+  permaLink: string;
+}
+
+export default function Posts({ permaLink, post }: PostsProps) {
+  usePostStore.setState({ loading: false, post }, true);
+
+  const { title, slug } = post;
+
+  return (
+    <Layout seo={{ permaLink, title }}>
+      <PostContent />
+      {/* <Recommendations entitie="posts" slugToExclude={slug} /> */}
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostsProps> = async (ctx) => {
+  try {
+    const permaLink = ctx.params?.slug as string;
+
+    const post = await getPostBySlug(permaLink);
+
+    return { props: { permaLink, post } };
+  } catch (error) {
+    return { notFound: true };
+  }
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getPostsSlug();
+
+  return {
+    paths: posts.map(({ slug }) => ({ params: { slug } })),
+    fallback: "blocking",
+  };
+};
